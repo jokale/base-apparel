daf792e3690697d82b674245ce37783c656d150b
jokale <kalemajoanna@gmail.com>
2021-01-06 12:44:35 +0000

    fix p text and add styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
     <div>
       <h1 id="first">WE'RE</h1>
       <h1 id="second">COMING <br></br> SOON</h1>
-      <p id="p">Hello fellow shoppers! We're currently building our new <br></br> fashion store. Add your email below to stay up to date with <br></br> annoucements and our launch details.</p>
+      <p id="p">Hello fellow shoppers! We're currently building our new </p>
+     <br></br> <p id="p1"> fashion store. Add your email below to stay up to date with </p>
+     <br></br><p id="p2"> annoucements and our launch details.</p>
       <form>
         <input placeholder="Email Address"></input>
       </form>
