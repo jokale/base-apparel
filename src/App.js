262ddbf78620eadc7c7f957bb99426b93d9253db
jokale <kalemajoanna@gmail.com>
2021-01-06 12:21:04 +0000

    rerender hero image and move text

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
   render(){
     return (
     <div>
-      <h1>WE'RE</h1>
-      <h1>COMING <br></br> SOON</h1>
-      <p>Hello fellow shoppers! We're currently building our new <br></br> fashion store. Add your email below to stay up to date with <br></br> annoucements and our launch details.</p>
+      <h1 id="first">WE'RE</h1>
+      <h1 id="second">COMING <br></br> SOON</h1>
+      <p id="p">Hello fellow shoppers! We're currently building our new <br></br> fashion store. Add your email below to stay up to date with <br></br> annoucements and our launch details.</p>
       <form>
         <input placeholder="Email Address"></input>
       </form>
