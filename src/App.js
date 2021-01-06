058a3db2949098b405069b7f7fc1944c1ed74696
jokale <kalemajoanna@gmail.com>
2021-01-06 11:34:08 +0000

    add text and form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App extends React.Component {
   render(){
     return (
     <div>
-      <h2>Hello</h2>
+      <h1>WE'RE</h1>
+      <h1>COMING <br></br> SOON</h1>
+      <p>Hello fellow shoppers! We're currently building our new <br></br> fashion store. Add your email below to stay up to date with <br></br> annoucements and our launch details.</p>
+      <form>
+        <input placeholder="Email Address"></input>
+      </form>
     </div>
   );
   }
