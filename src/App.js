662335e6d24252641721ebf35e8e955571f1777a
jokale <kalemajoanna@gmail.com>
2021-01-07 11:31:11 +0000

    add form validation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import logo from './logo.svg';
 import hero from './hero.jpg';
 import arrow from './arrow.svg';
 import desktop from './desktop.svg';
-import error from './error.svg'
+// import error from './error.svg'
 
 class App extends React.Component {
   
   
-  handleClick = (e) => {
+   handleClick (e) {
+    var x = document.forms["form"]["email"].value;
 
-  console.log(e.type);
-  alert('hey')
+    if (x === "") {
+      alert("Name must be filled out");
+      return(
+   
+        <div><h1>Please provide a valid email</h1>
+           </div>
+      )
+    }
+    else {      
+    
+      alert("Thanks we will email you when the store is open!")
+
+      
+      
+
+    }
   }
   
   
@@ -25,7 +40,7 @@ class App extends React.Component {
      <br></br> <p id="p1"> fashion store. Add your email below to stay up to date with </p>
      <br></br><p id="p2"> annoucements and our launch details.</p>
       <form name="form">
-        <input placeholder="Email Address" name="email"></input>
+        <input placeholder="Email Address" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" ></input>
         <button id="button" type="submit" onClick={this.handleClick.bind(this)}></button>
       </form>
       <img src={logo} id="logo" alt="logo" />
